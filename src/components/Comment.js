46f1c401664a3350151459ddf064b39fcc3d848f
Yogeshwar J <yogeshj939@gmail.com>
2022-09-30 21:19:47 +0530

    First comment in comment section

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,6 +4,7 @@ export default function Comments(){
     return(
         <div>
             <h1>This is Comment Section. Say Hi to Comments</h1>
+            <p>This page is awesome. able to book hotels at cheap price</p>
         </div>
     )
 }
